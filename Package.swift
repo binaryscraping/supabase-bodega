08c597798deae8b740089ac8a925a4ff5349dcfa
Guilherme Souza <grsouza@pm.me>
2022-09-24 07:37:49 -0300

    Implement all methods from StorageEngine

--- Package.swift
+++ Package.swift
@@ -4,33 +4,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "supabase-bodega",
-    platforms: [
-      .iOS(.v15),
-      .macOS(.v12),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SupabaseBodega",
-            targets: ["SupabaseBodega"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/mergesort/Bodega", from: "2.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SupabaseBodega",
-            dependencies: [
-              "Bodega"
-            ]
-        ),
-        .testTarget(
-            name: "SupabaseBodegaTests",
-            dependencies: ["SupabaseBodega"]
-        ),
-    ]
+  name: "supabase-bodega",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v12),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "SupabaseBodega",
+      targets: ["SupabaseBodega"]
+    ),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/mergesort/Bodega", from: "2.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "SupabaseBodega",
+      dependencies: [
+        "Bodega",
+      ]
+    ),
+    .testTarget(
+      name: "SupabaseBodegaTests",
+      dependencies: ["SupabaseBodega"]
+    ),
+  ]
 )
--- Sources/SupabaseBodega/SupabaseBodega.swift
+++ Sources/SupabaseBodega/SupabaseBodega.swift
@@ -2,21 +2,53 @@ import Bodega
 import Foundation
 
 public actor SupabaseStorageEngine: StorageEngine {
-
-  private struct StoredValue: Encodable {
+  private struct StoredValue: Codable {
     let key: String
     let data: Data
   }
 
-  public enum Error: Swift.Error {
+  public enum Error: LocalizedError {
     case unaccetableStatusCode(_ code: Int)
+
+    public var errorDescription: String? {
+      switch self {
+      case let .unaccetableStatusCode(code):
+        return "Unaccetable status code: \(code)"
+      }
+    }
   }
 
-  let session: URLSession = .shared
+  let session: URLSession
   let url: URL
 
-  public init(table: String) {
-    url = URL(string: "http://localhost:5435/v1/rest/\(table)")!
+  let encoder: JSONEncoder
+  let decoder: JSONDecoder
+
+  public init(url: URL, table: String, apiKey: String) {
+    let configuration = URLSessionConfiguration.default
+    configuration.httpAdditionalHeaders = [
+      "apikey": apiKey,
+      "Authorization": "Bearer \(apiKey)",
+      "Content-Type": "application/json",
+    ]
+    session = URLSession(configuration: configuration)
+    self.url = url.appendingPathComponent("rest/v1/\(table)")
+
+    let encoder = JSONEncoder()
+    encoder.keyEncodingStrategy = .convertToSnakeCase
+
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+    let dateFormatter = ISO8601DateFormatter()
+    dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    decoder.dateDecodingStrategy = .custom { decoder in
+      let string = try decoder.singleValueContainer().decode(String.self)
+      return dateFormatter.date(from: string)!
+    }
+
+    self.encoder = encoder
+    self.decoder = decoder
   }
 
   public func write(_ data: Data, key: Bodega.CacheKey) async throws {
@@ -28,88 +60,311 @@ public actor SupabaseStorageEngine: StorageEngine {
       StoredValue(key: key.value, data: data)
     }
 
-    var request = URLRequest(url: url)
-    request.setValue("resolution=merge-duplicates", forHTTPHeaderField: "Prefer")
-    request.httpMethod = "POST"
-    request.httpBody = try? JSONEncoder().encode(body)
+    let request = Request(
+      method: "POST",
+      body: try? encoder.encode(body),
+      headers: [
+        "Prefer": "resolution=merge-duplicates",
+      ]
+    )
 
-    let (_, response) = try await session.data(for: request)
-    try validate(response)
+    try await send(request)
   }
 
   public func read(key: Bodega.CacheKey) async -> Data? {
-    var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-    components.queryItems = [
-      URLQueryItem(name: "select", value: "data"),
-      URLQueryItem(name: "key", value: "eq.\(key.value)")
-    ]
-    let url = components.url!
-    var request = URLRequest(url: url)
-    request.httpMethod = "GET"
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "data"),
+        URLQueryItem(name: "key", value: "eq.\(key.value)"),
+      ]
+    )
 
     do {
-      let (data, response) = try await session.data(for: request)
-      try validate(response)
-      return data
+      let (data, _) = try await send(request)
+      struct Container: Decodable {
+        let data: Data
+      }
+      let container = try decoder.decode([Container].self, from: data)
+      return container.first?.data
     } catch {
+      print(error)
       return nil
     }
   }
 
   public func read(keys: [CacheKey]) async -> [Data] {
-    []
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "data"),
+        URLQueryItem(name: "key", value: "in.(\(keys.map(\.value).joined(separator: ",")))"),
+      ]
+    )
+
+    do {
+      let (data, _) = try await send(request)
+      struct Container: Decodable {
+        let data: Data
+      }
+      let container = try decoder.decode([Container].self, from: data)
+      return container.map(\.data)
+    } catch {
+      print(error)
+      return []
+    }
   }
 
-  public func readDataAndKeys(keys: [Bodega.CacheKey]) async -> [(key: Bodega.CacheKey, data: Data)] {
-    []
+  public func readDataAndKeys(keys: [Bodega.CacheKey]) async
+    -> [(key: Bodega.CacheKey, data: Data)]
+  {
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "key,data"),
+        URLQueryItem(name: "key", value: "in.(\(keys.map(\.value).joined(separator: ",")))"),
+      ]
+    )
+
+    do {
+      let (data, _) = try await send(request)
+      let values = try decoder.decode([StoredValue].self, from: data)
+
+      return values.map {
+        (CacheKey(verbatim: $0.key), $0.data)
+      }
+    } catch {
+      print(error)
+      return []
+    }
   }
 
   public func readAllData() async -> [Data] {
-    []
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "data"),
+      ]
+    )
+    do {
+      let (data, _) = try await send(request)
+      struct Container: Decodable {
+        let data: Data
+      }
+      let container = try decoder.decode([Container].self, from: data)
+      return container.map(\.data)
+    } catch {
+      print(error)
+      return []
+    }
   }
 
   public func readAllDataAndKeys() async -> [(key: CacheKey, data: Data)] {
-    []
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "key,data"),
+      ]
+    )
+
+    do {
+      let (data, _) = try await send(request)
+      let values = try decoder.decode([StoredValue].self, from: data)
+
+      return values.map {
+        (CacheKey(verbatim: $0.key), $0.data)
+      }
+    } catch {
+      print(error)
+      return []
+    }
   }
 
   public func remove(key: CacheKey) async throws {
+    let request = Request(
+      method: "DELETE",
+      query: [
+        URLQueryItem(name: "key", value: "eq.\(key.value)"),
+      ]
+    )
 
+    try await send(request)
   }
 
   public func remove(keys: [CacheKey]) async throws {
+    let request = Request(
+      method: "DELETE",
+      query: [
+        URLQueryItem(name: "key", value: "in.(\(keys.map(\.value).joined(separator: ",")))"),
+      ]
+    )
 
+    try await send(request)
   }
 
   public func removeAllData() async throws {
+    let request = Request(
+      method: "DELETE",
+      query: [
+        URLQueryItem(name: "key", value: "neq.\(UUID().uuidString)"),
+      ]
+    )
+    try await send(request)
   }
 
   public func keyExists(_ key: Bodega.CacheKey) async -> Bool {
-    true
+    let request = Request(
+      method: "HEAD",
+      query: [
+        URLQueryItem(name: "key", value: "eq.\(key.value)"),
+      ],
+      headers: [
+        "Prefer": "count=exact",
+      ]
+    )
+
+    do {
+      let (_, response) = try await send(request)
+      guard let contentRange = response.value(forHTTPHeaderField: "Content-Range") else {
+        throw URLError(.badServerResponse)
+      }
+
+      let count = contentRange.split(separator: "/").last.flatMap { Int($0) } ?? 0
+      return count > 0
+    } catch {
+      print(error)
+      return false
+    }
   }
 
   public func keyCount() async -> Int {
-    1
+    let request = Request(
+      method: "HEAD",
+      headers: [
+        "Prefer": "count=exact",
+      ]
+    )
+
+    do {
+      let (_, response) = try await send(request)
+      guard let contentRange = response.value(forHTTPHeaderField: "Content-Range") else {
+        throw URLError(.badServerResponse)
+      }
+
+      let count = contentRange.split(separator: "/").last
+      return count.flatMap { Int($0) } ?? 0
+    } catch {
+      print(error)
+      return 0
+    }
   }
 
   public func allKeys() async -> [Bodega.CacheKey] {
-    []
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "key"),
+      ]
+    )
+    do {
+      let (data, _) = try await send(request)
+
+      struct KeyResponse: Decodable {
+        let key: String
+      }
+
+      let keys = try decoder.decode([KeyResponse].self, from: data)
+
+      return keys.map { CacheKey(verbatim: $0.key) }
+    } catch {
+      return []
+    }
   }
 
   public func createdAt(key: Bodega.CacheKey) async -> Date? {
-    nil
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "created_at"),
+        URLQueryItem(name: "key", value: "eq.\(key.value)"),
+      ]
+    )
+
+    do {
+      let (data, _) = try await send(request)
+      struct Container: Decodable {
+        let createdAt: Date
+      }
+      let values = try decoder.decode([Container].self, from: data)
+      return values.first?.createdAt
+    } catch {
+      print(error)
+      return nil
+    }
   }
 
   public func updatedAt(key: Bodega.CacheKey) async -> Date? {
-    nil
+    let request = Request(
+      method: "GET",
+      query: [
+        URLQueryItem(name: "select", value: "updated_at"),
+        URLQueryItem(name: "key", value: "eq.\(key.value)"),
+      ]
+    )
+
+    do {
+      let (data, _) = try await send(request)
+      struct Container: Decodable {
+        let updatedAt: Date
+      }
+      let values = try decoder.decode([Container].self, from: data)
+      return values.first?.updatedAt
+    } catch {
+      print(error)
+      return nil
+    }
   }
 
-  private func validate(_ response: URLResponse) throws {
+  @discardableResult
+  private func send(_ request: Request) async throws -> (Data, HTTPURLResponse) {
+    let (data, response) = try await session.data(for: request, withURL: url)
+    let httpResponse = try validate(response)
+    return (data, httpResponse)
+  }
+
+  private func validate(_ response: URLResponse) throws -> HTTPURLResponse {
     guard let httpResponse = response as? HTTPURLResponse else {
       throw URLError(.badServerResponse)
     }
 
-    guard 200..<300 ~= httpResponse.statusCode else {
+    guard 200 ..< 300 ~= httpResponse.statusCode else {
       throw Error.unaccetableStatusCode(httpResponse.statusCode)
     }
+
+    return httpResponse
+  }
+}
+
+struct Request {
+  let method: String
+  var query: [URLQueryItem] = []
+  var body: Data?
+  var headers: [String: String] = [:]
+}
+
+extension URLSession {
+  func data(for request: Request, withURL url: URL) async throws -> (Data, URLResponse) {
+    var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+    if !request.query.isEmpty {
+      components.queryItems = request.query
+    }
+    let url = components.url!
+    var urlRequest = URLRequest(url: url)
+    urlRequest.httpBody = request.body
+    urlRequest.httpMethod = request.method
+    request.headers.forEach {
+      urlRequest.setValue($0.value, forHTTPHeaderField: $0.key)
+    }
+    return try await data(for: urlRequest)
   }
 }
